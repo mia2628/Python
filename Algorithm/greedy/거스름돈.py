6b473f0442ba7b2687e94260c90735de34b0e155
mia2628 <50148752+mia2628@users.noreply.github.com>
2021-01-02 23:44:50 +0900

    greedy 거스름돈

---
+++ Algorithm/greedy/거스름돈.py
@@ -0,0 +1,12 @@
+﻿n = int(input())
+
+count = 0
+coin_types = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+
+for coin_type in coin_types:
+    count += (n // coin_type)
+    n -= (n // coin_type * coin_type)
+    if (n == 0):
+      break
+    
+print(count)
