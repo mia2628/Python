a569ffcef71c574974b2e0d4a34b6e8dafdc11c6
mia2628 <50148752+mia2628@users.noreply.github.com>
2021-01-27 00:04:26 +0900

    두개 뽑아서 더하기

---
+++ Algorithm/Sort-1/두개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    
+    num = []
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            num.append(numbers[i] + numbers[j])
+            
+    num = list(set(num))
+    num.sort()
+    return num
