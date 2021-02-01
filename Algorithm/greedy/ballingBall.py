279570da089fd9c4c07ec22c8e32c593ca6b5e60
mia2628 <50148752+mia2628@users.noreply.github.com>
2021-02-01 16:35:25 +0900

    greedy 볼링공 고르기

---
+++ Algorithm/greedy/ballingBall.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+array = [0] * 11 #1부터 10까지의 무게를 담을 수 있는 리스트
+for x in data:
+  array[x] += 1 #각 무게에 해당하는 볼링공의 개수 카운트
+
+result = 0
+#1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m+1):
+  n -= array[i] #무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+  result += array[i] * n # B가 선택하는 경우의 수와 곱하기
+
+print(result)
